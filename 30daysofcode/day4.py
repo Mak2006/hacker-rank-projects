2ab58a8d752f59f3d63cd65981a7de6708abf8a4
Mak2006 <gmcct.adm@gmail.com>
2020-06-08 18:30:44 +0100

    finished amIold

--- 30daysofcode/day4.py
+++ 30daysofcode/day4.py
@@ -12,23 +12,35 @@ class Person:
         # do not like that Person can be created with 0 age !
         if(initialAge < 0 ):
             self.age = 0; #Private variable created
-            id +=1 # increament the id.
-            print("Age is not valid")
+            Person.id = Person.id + 1 # increament the id.
+            print("Age is not valid, setting age to 0.")
         else:
             self.age = initialAge # SET it to the person age
 
-
     def amIOld(self):
         # Do some computations in here and print out the correct statement to the console
+        a = self.age
+        if(a < 13):
+            print("You are young.")
+        elif (a >=13 and a < 18):
+            print("You are a teenager.")
+        else:
+            print("You are old.")
+
     def yearPasses(self):
         # Increment the age of the person in here
+        self.age = self.age + 1;
+        #print("setting age{}".format(self.age))
 
-t = int(input())
-for i in range(0, t):
-    age = int(input())
-    p = Person(age)
-    p.amIOld()
-    for j in range(0, 3):
-        p.yearPasses()
-    p.amIOld()
-    print("")
+
+if __name__ == '__main__':
+    print(" Give first the number of input, then the inputs")
+    t = int(input())
+    for i in range(0, t):
+        age = int(input())
+        p = Person(age)
+        p.amIOld()
+        for j in range(0, 3):
+            p.yearPasses()
+        p.amIOld()
+        print("")
